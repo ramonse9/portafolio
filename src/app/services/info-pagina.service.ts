8096426942861b27d548e930fe6ed2e93f9ed038
Ramon Antonio Guzman <ramonantoniojr@gmail.com>
2020-05-28 14:24:34 -0600

    ultimo checkin

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina-interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  constructor(private http: HttpClient) {
+
+    console.log('Servicio de info pagina listo');
+
+    // Leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) => {
+        // console.log( resp );
+
+        this.cargada = true;
+        this.info = resp;
+        console.log( resp );
+        // console.log( resp['twitter'] );
+        // console.log( resp['email']);
+
+      });
+
+
+   }
+}
